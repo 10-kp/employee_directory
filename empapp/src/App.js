997f84bab11326b096a5f891e68d77814b5b48e3
Kp <kp_1@outlook.com>
2021-06-03 21:26:19 +0400

    Updated components, 2 js files, app.js

--- empapp/src/App.js
+++ empapp/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import EmployeeList from './components/EmployeeList';
+
 import './App.css';
+
+const filteredEmployees = data.filter((employee) =>
+  employee.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className='App-logo' alt='logo' /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+
+      <EmployeeList data={filteredEmployees} />
     </div>
   );
 }
